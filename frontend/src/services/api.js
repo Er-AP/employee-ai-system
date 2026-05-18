2a0baad40cc4aa1ead72bf1db02f8995a8d7a353
Er-AP <ayushpatel.eng@gmail.com>
2026-05-18 22:50:38 +0530

    latest update in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://employee-ai-backend-rpur.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
