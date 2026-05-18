8bd5ff5864e2f87d69d6d6fe76a1a208b4efcfa0
Er-AP <ayushpatel.eng@gmail.com>
2026-05-18 22:41:21 +0530

    Fixed CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/employees", employeeRoutes);
